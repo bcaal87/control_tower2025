e87bdd486217786577b72568de79fb4970bb25db
Byron Caal <caal97@gmail.com>
2025-05-02 14:30:17 -0600

    db conexion

--- CongresoUMG/obj/Debug/net8.0/CongresoUMG.AssemblyInfo.cs
+++ CongresoUMG/obj/Debug/net8.0/CongresoUMG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CongresoUMG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd3585c8d2e97e09431212f12ea23fdfd987889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57bc62ff74bccd9b3f3318d334693b64ba0c9e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CongresoUMG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CongresoUMG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
