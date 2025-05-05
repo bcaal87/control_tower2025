de738ff18bac9d72fb7e18323e04dc058293634b
Byron Caal <caal97@gmail.com>
2025-05-05 15:22:05 -0600

    new sponsor

--- CongresoUMG/obj/Debug/net8.0/CongresoUMG.AssemblyInfo.cs
+++ CongresoUMG/obj/Debug/net8.0/CongresoUMG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CongresoUMG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fecd33353cda0c2aa3da1025714b52fc5addbd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0d3ddd8ce931a38d9d20f1d8d8fe8ba50b68d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CongresoUMG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CongresoUMG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
